4c4ebba5d45d2cad00d0493d277812ce911a5026
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-10-24 13:37:00 +0200

    FIrst commit

---
+++ TiltEm/CelestialBody_CBUpdate.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace TiltEm
+{
+    /// <summary>
+    /// This harmony patch is intended to not allow you generating contracts in case you don't have the contract lock
+    /// </summary>
+    [HarmonyPatch(typeof(CelestialBody))]
+    [HarmonyPatch("CBUpdate")]
+    public class CelestialBody_CBUpdate
+    {
+        [HarmonyPostfix]
+        private static void PostFixCBUpdate(CelestialBody __instance)
+        {
+            if (!__instance.bodyName.Contains("Kerbin")) return;
+
+            if (__instance.bodyTransform.rotation.eulerAngles.x == 0)
+            {
+                __instance.bodyTransform.Rotate(new Vector3(45, 0, 0));
+            }
+        }
+    }
+}
---
+++ TiltEm/TiltEm.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using System.Reflection;
+using UnityEngine;
+
+namespace TiltEm
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class TiltEm : MonoBehaviour
+    {
+        public static HarmonyInstance HarmonyInstance = HarmonyInstance.Create("TiltEm");
+
+        public void Awake()
+        {
+            DontDestroyOnLoad(this);
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
