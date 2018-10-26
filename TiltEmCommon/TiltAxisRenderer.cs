273900273c95db4a5d792f8b14e586e821c6dbac
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-10-26 18:52:30 +0200

    Cleanup line

--- TiltEmCommon/TiltAxisRenderer.cs
+++ TiltEmCommon/TiltAxisRenderer.cs
@@ -7,7 +7,6 @@ namespace TiltEmCommon
     [KSPAddon(KSPAddon.Startup.TrackingStation, true)]
     public class TiltAxisRenderer : MonoBehaviour
     {
-        private static CelestialBody _targetBody;
         private static VectorLine _line;
 
         private static readonly List<Vector3> Points = new List<Vector3> { new Vector3(), new Vector3() };
@@ -19,16 +18,14 @@ namespace TiltEmCommon
 
         public void Update()
         {
+            if (_line != null)
+                VectorLine.Destroy(ref _line);
+
             if (HighLogic.LoadedScene != GameScenes.TRACKSTATION || PlanetariumCamera.fetch.target.celestialBody == null) return;
 
             var body = PlanetariumCamera.fetch.target.celestialBody;
 
-            if (_targetBody != body)
-                VectorLine.Destroy(ref _line);
-
-            _targetBody = body;
-
-            var scale = (float)(body.Radius * 0.00025);
+            var scale = (float)(body.Radius * 0.0025);
 
             Points[0] = body.scaledBody.transform.position + body.scaledBody.transform.up * scale; //top
             Points[1] = body.scaledBody.transform.position + (-1 * body.scaledBody.transform.up) * scale; //bottom
