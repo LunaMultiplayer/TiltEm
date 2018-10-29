e5ce659e7282bebb5131c40e6d646426a5462786
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-10-30 00:32:30 +0100

    Added help method

--- TiltEm/TiltEm.cs
+++ TiltEm/TiltEm.cs
@@ -107,13 +107,21 @@ namespace TiltEm
                 body.rotation.swizzle.FrameVectors(out body.BodyFrame.X, out body.BodyFrame.Y, out body.BodyFrame.Z);
             }
         }
-        
+
         /// <summary>
-        /// Does the same as Transform.Rotate but against a given quaternion
+        /// Does the same as Transform.Rotate but against a given quaternion and against WORLD space
         /// </summary>
         /// <param name="quaternion">Quaternion to apply the rotation to</param>
         /// <param name="tilt">Rotation to apply</param>
         /// <returns>Rotated Quaternion</returns>
         public static Quaternion ApplySpaceRotation(Quaternion quaternion, Vector3 tilt) => quaternion * (Quaternion.Inverse(quaternion) * Quaternion.Euler(tilt)) * quaternion;
+
+        /// <summary>
+        /// Does the same as Transform.Rotate but against a given quaternion and against LOCAL space
+        /// </summary>
+        /// <param name="quaternion">Quaternion to apply the rotation to</param>
+        /// <param name="tilt">Rotation to apply</param>
+        /// <returns>Rotated Quaternion</returns>
+        public static Quaternion ApplyLocalRotation(Quaternion quaternion, Vector3 tilt) => quaternion * Quaternion.Euler(tilt);
     }
 }
