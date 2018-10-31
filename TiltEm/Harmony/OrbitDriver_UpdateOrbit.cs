bafaf38ca820f3965fa17861f7d8aba0fec391c9
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-10-31 11:54:39 +0100

    Fix orbits

---
+++ TiltEm/Harmony/OrbitDriver_UpdateOrbit.cs
@@ -0,0 +1,39 @@
+﻿using Harmony;
+
+// ReSharper disable All
+
+namespace TiltEm.Harmony
+{
+    /// <summary>
+    /// This harmony patch is intended to update the orbits and apply them the planet tilt
+    /// </summary>
+    [HarmonyPatch(typeof(OrbitDriver))]
+    [HarmonyPatch("UpdateOrbit")]
+    internal class OrbitDriver_UpdateOrbit
+    {
+        private static Vector3d Tilt;
+
+        [HarmonyPrefix]
+        private static void PreFixUpdateOrbit(OrbitDriver __instance, ref bool __state)
+        {
+            if (__instance.vessel == FlightGlobals.ActiveVessel) return;
+
+            __state = false;
+            if (__instance.referenceBody && __instance.referenceBody.inverseRotation && TiltEm.TryGetTilt(__instance.referenceBody.bodyName, out Tilt))
+            {
+                __state = true;
+
+                TiltEmUtil.ApplyTiltToFrame(ref __instance.orbit.OrbitFrame, -1 * Tilt);
+            }
+        }
+
+        [HarmonyPostfix]
+        private static void PostFixUpdateOrbit(OrbitDriver __instance, ref bool __state)
+        {
+            if (__state)
+            {
+                TiltEmUtil.ApplyTiltToFrame(ref __instance.orbit.OrbitFrame, Tilt);
+            }
+        }
+    }
+}
