51d12739d9d00018a614aab0354a0f7bf95102c5
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-10-31 12:33:51 +0100

    Fix tilt of other vessel orbits

--- TiltEm/Harmony/OrbitDriver_UpdateOrbit.cs
+++ TiltEm/Harmony/OrbitDriver_UpdateOrbit.cs
@@ -23,7 +23,7 @@ namespace TiltEm.Harmony
             {
                 __state = true;
 
-                TiltEmUtil.ApplyTiltToFrame(ref __instance.orbit.OrbitFrame, -1 * Tilt);
+                TiltEmUtil.ApplyTiltToFrame(ref __instance.orbit.OrbitFrame, Tilt);
             }
         }
 
@@ -32,7 +32,7 @@ namespace TiltEm.Harmony
         {
             if (__state)
             {
-                TiltEmUtil.ApplyTiltToFrame(ref __instance.orbit.OrbitFrame, Tilt);
+                TiltEmUtil.ApplyTiltToFrame(ref __instance.orbit.OrbitFrame, -1 * Tilt);
             }
         }
     }
