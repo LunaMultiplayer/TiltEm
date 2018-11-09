a680b80585060fa520a7553121cc5bf79b3bcf51
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-11-09 17:19:33 +0100

    New way of applying tilt to packed vessels/planets

--- TiltEm/Harmony/OrbitPhysicsManager_SetRotatingFrame.cs
+++ TiltEm/Harmony/OrbitPhysicsManager_SetRotatingFrame.cs
@@ -16,20 +16,20 @@ namespace TiltEm.Harmony
         {
             if (__instance.dominantBody.inverseRotation != rotatingFrameState)
             {
-                //For all the vessels in physics range, we must set the orbit mode to UPDATE for 1 frame. This is needed as otherwise their position won't be correct after
-                //switching the frame
-                for (var i = 0; i < FlightGlobals.VesselsLoaded.Count; i++)
-                {
-                    if (FlightGlobals.VesselsLoaded[i].orbitDriver.updateMode == OrbitDriver.UpdateMode.TRACK_Phys)
-                    {
-                        FlightGlobals.VesselsLoaded[i].orbitDriver.SetOrbitMode(OrbitDriver.UpdateMode.UPDATE);
-                        CallbackUtil.DelayedCallback(1, () => { FlightGlobals.VesselsLoaded[i].orbitDriver.SetOrbitMode(OrbitDriver.UpdateMode.TRACK_Phys); });
-                    }
-                }
-                
                 if (rotatingFrameState)
                 {
                     TiltEmUtil.RestorePlanetTilt(__instance.dominantBody);
+                    
+                    //For all the vessels in physics range, we must set the orbit mode to UPDATE for 1 frame. This is needed as otherwise their position won't be correct after
+                    //switching the frame
+                    for (var i = 0; i < FlightGlobals.VesselsLoaded.Count; i++)
+                    {
+                        if (FlightGlobals.VesselsLoaded[i].orbitDriver.updateMode == OrbitDriver.UpdateMode.TRACK_Phys && TiltEm.DebugSwitches[0])
+                        {
+                            FlightGlobals.VesselsLoaded[i].orbitDriver.SetOrbitMode(OrbitDriver.UpdateMode.UPDATE);
+                            CallbackUtil.DelayedCallback(1, () => { FlightGlobals.VesselsLoaded[i].orbitDriver.SetOrbitMode(OrbitDriver.UpdateMode.TRACK_Phys); });
+                        }
+                    }
                 }
                 else
                 {
