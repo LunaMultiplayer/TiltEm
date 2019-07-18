bffe275aefddc9296a1116b2b897d9a7e2a73bf9
gavazquez <gavazquez@gmail.com>
2019-07-18 11:58:07 +0200

    Easier localization

--- TiltEm/Harmony/KbAppPlanetParameters_CreatePhysicalCharacteristics.cs
+++ TiltEm/Harmony/KbAppPlanetParameters_CreatePhysicalCharacteristics.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 namespace TiltEm.Harmony
 {
     /// <summary>
-    /// This harmony patch is intended to display the axial tilt in the track station knowledge base
+    /// This harmony patch is intended to display the axial tilt in the tracking station knowledge base
     /// </summary>
     [HarmonyPatch(typeof(KbApp_PlanetParameters))]
     [HarmonyPatch("CreatePhysicalCharacteristics")]
@@ -17,21 +17,12 @@ namespace TiltEm.Harmony
     {
         private static string Title => Localizer.TryGetStringByTag("#autoLOC_TiltEm_AxialTilt", out var localizedTitle) ? localizedTitle : "Obliquity";
 
-        private static string GetLocalizedDegrees(string bodyName)
-        {
-            if (Localizer.TryGetStringByTag("#autoLOC_TiltEm_AxialTiltDisplay", out _))
-            {
-                return Localizer.Format("#autoLOC_TiltEm_AxialTiltDisplay", TiltEm.GetTiltForDisplay(bodyName));
-            }
-
-            return $"{TiltEm.GetTiltForDisplay(bodyName)} deg";
-        }
 
         [HarmonyPostfix]
         private static void PostfixCreatePhysicalCharacteristics(KbApp_PlanetParameters __instance, List<UIListItem> __result)
         {
             var uIListItem = __instance.cascadingList.CreateBody(Title, string.Concat(new string[] { "<color=#b8f4d1>",
-                GetLocalizedDegrees(__instance.currentBody.bodyName), "</color>" }));
+                $"{TiltEm.GetTiltForDisplay(__instance.currentBody.bodyName)}°", "</color>" }));
 
             __result.Add(uIListItem);
         }
